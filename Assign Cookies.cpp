0a03a3c5d9cb9d773ba407b3c90aefe17d221ffa
Dave Aditya <adityadave049@gmail.com>
2025-05-05 17:59:00 +0530

    Create Assign Cookies.cpp

---
+++ Assign Cookies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int r=0,l=0;
+        while(r<g.size() && l<s.size()){
+            if(g[r]<=s[l])
+            r++;
+            l++;
+        }
+        return r;
+        
+    }
+};
