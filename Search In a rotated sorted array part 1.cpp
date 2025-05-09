d9ab27413ee0c4d979ae163594e7520a568180c1
Dave Aditya <adityadave049@gmail.com>
2025-05-09 09:52:17 +0530

    Create Search In a rotated sorted array part 1.cpp

---
+++ Search In a rotated sorted array part 1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int n=arr.size();
+        int start=0,end=n-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target) return mid;
+            if(arr[mid]<=arr[end]){
+                if(arr[mid]<=target && target<=arr[end])
+                start=mid+1;
+                else end=mid-1;
+            }
+            else{
+                if(arr[mid]>=target && arr[start]<=target)
+                end=mid-1;
+                else start=mid+1;
+            }
+        }
+        return -1;
+        
+    }
+};
