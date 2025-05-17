21d8375002614d5a6c298466ebbd48ea807afc70
Dave Aditya <adityadave049@gmail.com>
2025-05-17 15:30:14 +0530

    Create Minimum bit flips to convert from A to B.cpp
    https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/

---
+++ Minimum bit flips to convert from A to B.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int ans=start^goal;
+        int cnt=0;
+
+        while(ans>0){
+            int rem=ans%2;
+            if(rem==1) cnt+=1;
+            ans/=2;
+        }
+        return cnt;
+    }
+};
