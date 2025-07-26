c52a7984ba4c6e68e56a56dbe6d9f40906ce563f
Dave Aditya <adityadave049@gmail.com>
2025-07-26 09:44:36 +0530

    Create Sum Root to Leaf Numbers.cpp
    https://leetcode.com/problems/sum-root-to-leaf-numbers/description/?envType=problem-list-v2&envId=depth-first-search

---
+++ Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+    void dfs(TreeNode* root, long long int &sum, int number){
+        if(root==NULL) return;
+
+        number=number*10+root->val;
+        if(root->left==NULL && root->right==NULL){
+            sum+=number;
+            number/=10;
+            return;
+        }
+
+        if(root->left) dfs(root->left,sum,number);
+        if(root->right) dfs(root->right,sum,number);
+        number/=10;
+    }
+    
+public:
+    int sumNumbers(TreeNode* root) {
+        long long int sum=0,number=0;
+
+        dfs(root,sum,number);
+        return sum;
+    }
+};
