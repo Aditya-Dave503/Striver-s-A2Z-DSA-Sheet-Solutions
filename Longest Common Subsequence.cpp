2ec57f67311df15475c6451a88c2e0d07ec69a84
Dave Aditya <adityadave049@gmail.com>
2025-10-22 17:31:16 +0530

    Implement Longest Common Subsequence algorithm
    https://leetcode.com/problems/longest-common-subsequence/description/

---
+++ Longest Common Subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int helper(int ind1,int ind2,string &text1, string &text2, vector<vector<int>>&dp){
+        if(ind1<0 || ind2<0) return 0;
+
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+
+        if(text1[ind1]==text2[ind2]) return dp[ind1][ind2]=1+helper(ind1-1,ind2-1,text1,text2,dp);
+
+        return dp[ind1][ind2]=max(helper(ind1-1,ind2,text1,text2,dp),helper(ind1,ind2-1,text1,text2,dp));
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.length();
+        int m=text2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(text1[ind1-1]==text2[ind2-1]) dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+                else
+                    dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+            }
+        }
+        return dp[n][m];
+    }
+};
